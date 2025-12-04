6b5d1aee87819adfc55437eeca4b699e88a5aa5d
void <void7535@gmail.com>
2025-12-04 16:53:43 -0600

    get code off of my buggy laptop

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# symphony compiler
+to use this project, open it in vs and run it, paste your code, and hit enter.
+
+example code (fibbonachi):
+	mov r1,1
+	fibbonachi_loop_label: ;comments look like this
+	add r2 r1 r2
+	add r1 r2 r1
+	jmp fibbonachi_loop_label
--- compiler.py
+++ compiler.py
@@ -1,17 +1,19 @@
-line = -1
+line_count = 0
+labels = []
 special_opcodes = ["nop","in","out","console","time_0","time_1","time_2","time_3", "counter","keyboard"]
 alu_opcodes = ["nand","or","and","nor","add","sub","xor","lsl","lsr","mul"]
-special_alu_opcodes = ["mov","neg","not","push","pop","call","ret"]
+special_alu_opcodes = ["mov","neg","not","push","pop","call","ret"] #these need to be handled on a case-by-case basis
 jumps = ["je","jne","jl","jge","jle","jg","jb","jae","jbe","ja"] 
 ram_opcodes = ["load_8","load_16","store_8","store_16","pload_8","pload_16","pstore_8","pstore_16"]
+output = ""
 
 def register(reg):
     try:
         reg = int(reg)
         if reg <-32768 or reg > 65535:
-            raise ValueError:
+            raise ValueError
         else:
-            return(reg)
+            return(bin(reg))
     except:
         if reg == "zr":
             return bin(0)
@@ -20,8 +22,8 @@ def register(reg):
         elif reg == "flags":
             return bin(15)
         reg = reg[1:-1]
-        elif reg<1 or 13<reg:
-            raise ValueError(f"line {line} Register r{reg} is out of bounds")
+        if reg<1 or 13<reg:
+            raise ValueError(f"line {line_count} Register r{reg} is out of bounds")
         else:
             return bin(int(reg))
 
@@ -46,8 +48,55 @@ def opcode(opc,reg1,regdest):
         elif opc == "pop":
             return([f"load_16 {regdest} [sp]","add sp sp 2"])
         elif opc == "call":
-            return(["counter flags","add flags flags 12","push flags",f"jmp {reg1}"])
+            return(["counter flags","add flags flags 16","push flags",f"jmp {reg1}"])
         elif opc == "ret":
             return("pop flags","jmp flags")
-def main_()
-    
+        else:
+            raise KeyError(f"line {line_count}: invalid opcode: {opc}")
+
+
+def main_(line):
+
+    line_count += 1
+    if ":" in line:
+        line_count -= 1
+        labels.append(line[0:-2])
+    else:
+        temp = line.find(" ") #temporaries just for splitting the string into opcodes and registers
+        temp2 = line[0:temp]
+        temp3 = line[temp+1:-1]
+        regcount = 0
+        reg1 = ""
+        reg2 = ""
+        regdest = ""
+        temp4 = ""
+        for char in temp3:
+            if char == ",":
+                 if regcount == 0:
+                     regdest = temp4
+                 elif regcount == 1:
+                     reg1 = temp4
+                 else:
+                     reg2 = temp4
+                 regcount += 1
+                 temp4 = ""
+            else:
+                temp4 += char
+        opc = opcode(temp2,reg1,regdest)
+        output *= 64
+        output.append(register(reg1))
+        output *= 16
+        output.append(register(reg2))
+        output *= 16
+        output.append(register(regdest))
+        
+        try:
+            opc = int(opc)
+            output.append(opc)
+        except:
+            for line in opc:
+                main_(line)
+
+
+
+
